89aeccf19a6f0ae87359f169d8de090d1d4e2826
Naser <nourany.na@gmail.com>
2017-10-28 15:23:57 +0330

    add okhttpClient

--- app/src/main/java/info/instagram/instagram/adapters/PostAdapter.java
+++ app/src/main/java/info/instagram/instagram/adapters/PostAdapter.java
@@ -1,12 +1,16 @@
 package info.instagram.instagram.adapters;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +20,12 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.internal.Streams;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -23,6 +33,12 @@ import java.util.Random;
 import info.instagram.instagram.R;
 import info.instagram.instagram.entities.ImagePost;
 import info.instagram.instagram.entities.Post;
+import info.instagram.instagram.fragments.PostFragment;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by Naser on 10/28/2017.
@@ -32,6 +48,8 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
 
     private List<Post> arrayList;
     private Context mContext;
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
 
     public PostAdapter(Context mContext, List<Post> imagePosts) {
         this.arrayList = imagePosts;
@@ -39,13 +57,13 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
         holder.mProfileName.setText(arrayList.get(position).getTitle());
         holder.mProfileName2.setText(arrayList.get(position).getTitle());
 
         String content = arrayList.get(position).getContent();
         content = content.replace(" src=\""," src=\"http://192.168.10.55:8282");
-        String text = "<html><head><meta charset=\"utf-8\"></head><body>" + content + "</body></html>";
+        String text = "<html><head><meta charset=\"utf-8\"></head><body>" + content + " <link rel=\"stylesheet\" href=\"192.168.10.55:8282/css/belowthefold.scss?v=bh30g4LDAv9UnKHOoHsIFPf_v7E\"> </body></html>";
         holder.webViewPost.loadData(text ,"text/html; charset=UTF-8" ,null);
 
        /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
@@ -69,28 +87,18 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
             holder.imgComment.setImageResource(R.drawable.ic_chat_bubble_outline_black_24dp);
         }*/
 
-        Random r = new Random();
-        int red=r.nextInt(255 - 0 + 1)+0;
-        int green=r.nextInt(255 - 0 + 1)+0;
-        int blue=r.nextInt(255 - 0 + 1)+0;
-
-        GradientDrawable draw = new GradientDrawable();
-        draw.setShape(GradientDrawable.RECTANGLE);
-        draw.setColor(Color.rgb(red,green,blue));
-        draw.setCornerRadius(2.0f);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            holder.layout.setBackground(draw);
-        }
-
-
         holder.imgOptions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+                GetPosts getPosts = new GetPosts();
+                getPosts.execute(String.valueOf(arrayList.get(position).getId()));
+
+
+                /*final AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
 
                 LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                View dialogView = inflater.inflate(R.layout.post_row_item, null);
+                View dialogView = inflater.inflate(R.layout.post_options, null);
                 alertDialog.setView(dialogView);
 
                 alertDialog.setTitle("Your title");
@@ -114,7 +122,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
                 });
 
                 AlertDialog dialog = alertDialog.create();
-                dialog.show();
+                dialog.show();*/
             }
         });
 
@@ -158,4 +166,65 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         }
     }
 
+    private class GetPosts extends AsyncTask<String, Void, String> {
+
+        private ProgressDialog progressDialog = new ProgressDialog(mContext);
+        protected void onPreExecute() {
+            progressDialog.setMessage("Downloading your data...");
+            progressDialog.show();
+            progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                public void onCancel(DialogInterface arg0) {
+                    GetPosts.this.cancel(true);
+                }
+            });
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String str="http://192.168.10.55:8282/api/PostApi/blog/comment";
+            OkHttpClient client = new OkHttpClient();
+
+            String json = "{" +
+                    "'postId' : " + params[0] + ","+
+                    "'comment': {" +
+                    "'IsAdmin':false," +
+                    "'Content': 'Test from android'," +
+                    "'Author': 'naser nourani'," +
+                    "'Email': 'na@gmail.com'," +
+                    "}" +
+                    "}";
+
+            RequestBody body = RequestBody.create(JSON, json);
+            Request request = new Request.Builder()
+                    .url(str)
+                    .post(body)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                return   response.body().string();
+            } catch (IOException e) {
+                Log.d("EXP", "doInBackground: " +  e.getMessage());
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (progressDialog.isShowing()){
+                progressDialog.dismiss();
+            }
+
+            if (result != null){
+
+//
+            }
+
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
 }
